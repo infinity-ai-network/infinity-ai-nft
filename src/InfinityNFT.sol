ac64a27985fc39cb137c5280794eab922c01e808
wxd56987 <672514143@qq.com>
2024-06-19 15:05:05 +0800

    feat: add merlin bsquared ai_layer rpc

--- src/InfinityNFT.sol
+++ src/InfinityNFT.sol
@@ -4,29 +4,34 @@ pragma solidity >=0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract InfinityNFT is ERC721, Ownable {
+    using Strings for uint256;
+
     constructor(
         string memory name_,
         string memory symbol_
     ) Ownable(msg.sender) ERC721(name_, symbol_) {}
 
-    mapping(uint256 => string) public urls;
+    mapping(uint256 => string) private _tokenURIs;
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "";
+    function setTokenURI(uint256 tokenId, string memory uri) internal {
+        _tokenURIs[tokenId] = uri;
     }
 
-    function mint(address to, uint256 tokenId, string memory url) external {
+    function mint(address to, uint256 tokenId, string memory uri) external {
         _mint(to, tokenId);
-        urls[tokenId] = url;
+        setTokenURI(tokenId, uri);
     }
 
     function burn(uint256 tokenId) external onlyOwner {
         _burn(tokenId);
     }
 
-    function tokenUrl(uint256 tokenId) external view returns (string memory) {
-        return urls[tokenId];
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_ownerOf(tokenId) != address(0), "ERC721Metadata: URI query for nonexistent token");
+
+        return _tokenURIs[tokenId];
     }
 }
--- test/InfinityNFT.t.sol
+++ test/InfinityNFT.t.sol
@@ -7,12 +7,22 @@ import {InfinityNFT} from "../src/InfinityNFT.sol";
 contract InfinityNFTTest is Test {
     InfinityNFT public infinityNFT;
     address deployer = address(0x11);
-    address whiteAddress1 = address(0x21);
-    address whiteAddress2 = address(0x31);
+    address toAddress = address(0x21);
     string _name = "InfinityNFT";
     string _symbol = "INF";
 
     function setUp() public {
         infinityNFT = new InfinityNFT(_name, _symbol);
+    }
+
+    function test_mintWithBytes() public {
+        uint256 tokenId = 2;
+        string memory url = '{"name": "InfinityNFT #0","description": "InfinityAI is an first one AIGC creator platform focused on Bitcoin ecosystem, integrating creation, trading, and community interaction.","image": "https://infinity-data-cdn.s3.ap-southeast-1.amazocampaign/ad9004e1-6d22-467c-b445-06af785bd1d4.png","external_url": "https://app.infinityai.network/","attributes": [{ "trait_type": "Trait Type", "value": "Trait Value" }]}';
+
+        infinityNFT.mint(toAddress, tokenId, url);
+
+        string memory r = infinityNFT.tokenURI(tokenId);
+
+        console.log(r);
     }
 }
